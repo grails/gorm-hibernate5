1231d9b4fab1e3856fad8336538f7558a5ec814a
graemerocher <graeme.rocher@gmail.com>
2016-06-28 15:11:21 +0200

    Use -refresh-dependencies

--- travis-build.sh
+++ travis-build.sh
@@ -4,12 +4,12 @@ EXIT_STATUS=0
 ./gradlew --stop
 
 
-./gradlew test -no-daemon -x gorm-hibernate5-spring-boot:test  || EXIT_STATUS=$?
+./gradlew test --refresh-dependencies -no-daemon -x gorm-hibernate5-spring-boot:test  || EXIT_STATUS=$?
 # if [[ $EXIT_STATUS -eq 0 ]]; then
 #     ./gradlew grails2-plugin:test -no-daemon || EXIT_STATUS=$?
 # fi
 if [[ $EXIT_STATUS -eq 0 ]]; then 
-    ./gradlew gorm-hibernate5-spring-boot:test -no-daemon || EXIT_STATUS=$?
+    ./gradlew gorm-hibernate5-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
 fi
 
 ./gradlew --stop
