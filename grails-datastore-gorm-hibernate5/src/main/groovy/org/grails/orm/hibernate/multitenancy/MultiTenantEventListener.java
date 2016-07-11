37a69f58062119cd73f5422834103bd555144972
Graeme Rocher <graeme.rocher@gmail.com>
2016-07-11 17:30:58 +0200

    Initial support for discriminator based multi tenancy

--- grails-datastore-gorm-hibernate5/src/main/groovy/org/grails/orm/hibernate/multitenancy/MultiTenantEventListener.java
+++ grails-datastore-gorm-hibernate5/src/main/groovy/org/grails/orm/hibernate/multitenancy/MultiTenantEventListener.java
@@ -1,7 +1,80 @@
 package org.grails.orm.hibernate.multitenancy;
 
+import grails.gorm.multitenancy.Tenants;
+import org.grails.datastore.mapping.core.Datastore;
+import org.grails.datastore.mapping.engine.event.AbstractPersistenceEvent;
+import org.grails.datastore.mapping.engine.event.AbstractPersistenceEventListener;
+import org.grails.datastore.mapping.engine.event.PersistenceEventListener;
+import org.grails.datastore.mapping.engine.event.PreInsertEvent;
+import org.grails.datastore.mapping.model.PersistentEntity;
+import org.grails.datastore.mapping.model.config.GormProperties;
+import org.grails.datastore.mapping.model.types.TenantId;
+import org.grails.datastore.mapping.query.Query;
+import org.grails.datastore.mapping.query.event.PostQueryEvent;
+import org.grails.datastore.mapping.query.event.PreQueryEvent;
+import org.grails.datastore.mapping.reflect.EntityReflector;
+import org.grails.orm.hibernate.HibernateDatastore;
+import org.grails.orm.hibernate.IHibernateTemplate;
+import org.grails.orm.hibernate.cfg.GrailsDomainBinder;
+import org.grails.orm.hibernate.query.HibernateHqlQuery;
+import org.grails.orm.hibernate.query.HibernateQuery;
+import org.hibernate.Session;
+import org.springframework.context.ApplicationEvent;
+
+import java.io.Serializable;
+
 /**
- * Created by graemerocher on 11/07/2016.
+ * @author Graeme Rocher
+ * @since 6.0
  */
-public class MultiTenantEventListener {
+public class MultiTenantEventListener implements PersistenceEventListener {
+    @Override
+    public boolean supportsEventType(Class<? extends ApplicationEvent> eventType) {
+        return PreQueryEvent.class.isAssignableFrom(eventType) || PostQueryEvent.class.isAssignableFrom(eventType) || PreInsertEvent.class.isAssignableFrom(eventType);
+    }
+
+    @Override
+    public boolean supportsSourceType(Class<?> sourceType) {
+        return HibernateDatastore.class.isAssignableFrom(sourceType);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if(supportsEventType(event.getClass()) && supportsSourceType(event.getSource().getClass())) {
+            HibernateDatastore hibernateDatastore = (HibernateDatastore) event.getSource();
+            if(event instanceof  PreQueryEvent) {
+                PreQueryEvent preQueryEvent = (PreQueryEvent) event;
+                Query query = preQueryEvent.getQuery();
+
+                PersistentEntity entity = query.getEntity();
+                if(entity.isMultiTenant()) {
+                    HibernateMultiTenancySupport.enableFilter(hibernateDatastore, entity);
+                }
+            }
+            else if(event instanceof PostQueryEvent) {
+                PostQueryEvent postQueryEvent = (PostQueryEvent) event;
+                Query query = postQueryEvent.getQuery();
+                PersistentEntity entity = query.getEntity();
+                if(entity.isMultiTenant()) {
+                    HibernateMultiTenancySupport.disableFilter(hibernateDatastore, entity);
+                }
+            }
+            else if(event instanceof PreInsertEvent) {
+                PreInsertEvent preInsertEvent = (PreInsertEvent) event;
+                PersistentEntity entity = preInsertEvent.getEntity();
+                if(entity.isMultiTenant()) {
+                    TenantId tenantId = entity.getTenantId();
+                    EntityReflector reflector = entity.getReflector();
+                    Serializable currentId = Tenants.currentId(hibernateDatastore.getClass());
+                    reflector.setProperty(preInsertEvent.getEntityObject(), tenantId.getName(), currentId);
+                }
+            }
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return DEFAULT_ORDER;
+    }
 }
+
--- grails-datastore-gorm-hibernate5/src/test/groovy/org/grails/orm/hibernate/connections/MultiTenantSpec.groovy
+++ grails-datastore-gorm-hibernate5/src/test/groovy/org/grails/orm/hibernate/connections/MultiTenantSpec.groovy
@@ -1,7 +1,130 @@
 package org.grails.orm.hibernate.connections
 
+import grails.gorm.MultiTenant
+import grails.gorm.multitenancy.Tenants
+import grails.persistence.Entity
+import org.grails.datastore.gorm.GormEntity
+import org.grails.datastore.mapping.core.DatastoreUtils
+import org.grails.datastore.mapping.multitenancy.exceptions.TenantNotFoundException
+import org.grails.datastore.mapping.multitenancy.resolvers.SystemPropertyTenantResolver
+import org.grails.orm.hibernate.HibernateDatastore
+import org.hibernate.Session
+import org.hibernate.SessionFactory
+import org.hibernate.dialect.H2Dialect
+import org.springframework.orm.hibernate5.SessionHolder
+import org.springframework.transaction.support.TransactionSynchronizationManager
+import spock.lang.Specification
+
 /**
  * Created by graemerocher on 11/07/2016.
  */
-class MultiTenantSpec {
+class MultiTenantSpec extends Specification {
+    void "Test a database per tenant multi tenancy"() {
+        setup:"A configuration for multiple data sources"
+        Map config = [
+                "grails.gorm.multiTenancy.mode":"MULTI",
+                "grails.gorm.multiTenancy.tenantResolverClass":SystemPropertyTenantResolver,
+                'dataSource.url':"jdbc:h2:mem:grailsDB;MVCC=TRUE;LOCK_TIMEOUT=10000",
+                'dataSource.dbCreate': 'update',
+                'dataSource.dialect': H2Dialect.name,
+                'dataSource.formatSql': 'true',
+                'dataSource.logSql': 'true',
+                'hibernate.flush.mode': 'COMMIT',
+                'hibernate.cache.queries': 'true',
+                'hibernate.hbm2ddl.auto': 'create',
+        ]
+
+        HibernateDatastore datastore = new HibernateDatastore(DatastoreUtils.createPropertyResolver(config), MultiTenantAuthor )
+
+
+        def sessionFactory = datastore.sessionFactory
+        def session = sessionFactory.openSession()
+        TransactionSynchronizationManager.bindResource(sessionFactory, new SessionHolder(session))
+
+        when:"no tenant id is present"
+        MultiTenantAuthor.list()
+
+
+        then:"An exception is thrown"
+        thrown(TenantNotFoundException)
+
+        when:"no tenant id is present"
+        def author = new MultiTenantAuthor(name: "Stephen King")
+        author.save(flush:true)
+
+        then:"An exception is thrown"
+        !author.errors.hasErrors()
+        thrown(TenantNotFoundException)
+
+        when:"A tenant id is present"
+        session.clear()
+        System.setProperty(SystemPropertyTenantResolver.PROPERTY_NAME, "moreBooks")
+
+        then:"the correct tenant is used"
+        MultiTenantAuthor.count() == 0
+
+        when:"An object is saved"
+        new MultiTenantAuthor(name: "Stephen King").save(flush: true)
+
+        then:"The results are correct"
+        MultiTenantAuthor.findAll("from MultiTenantAuthor a").size() == 1
+        MultiTenantAuthor.count() == 1
+
+        when:"An a transaction is used"
+        MultiTenantAuthor.withTransaction{
+            new MultiTenantAuthor(name: "James Patterson").save(flush:true)
+        }
+
+        then:"The results are correct"
+        MultiTenantAuthor.count() == 2
+
+        when:"The tenant id is switched"
+        System.setProperty(SystemPropertyTenantResolver.PROPERTY_NAME, "books")
+
+        then:"the correct tenant is used"
+        MultiTenantAuthor.count() == 0
+        MultiTenantAuthor.findAll("from MultiTenantAuthor a").size() == 0
+        MultiTenantAuthor.withTenant("moreBooks").count() == 2
+        MultiTenantAuthor.withTenant("moreBooks") { String tenantId, Session s ->
+            assert s != null
+            MultiTenantAuthor.count() == 2
+        }
+        Tenants.withId("books") {
+            MultiTenantAuthor.count() == 0
+        }
+        Tenants.withId("moreBooks") {
+            MultiTenantAuthor.count() == 2
+        }
+        Tenants.withCurrent {
+            MultiTenantAuthor.count() == 0
+        }
+
+        when:"each tenant is iterated over"
+        Map tenantIds = [:]
+        MultiTenantAuthor.eachTenant { String tenantId ->
+            tenantIds.put(tenantId, MultiTenantAuthor.count())
+        }
+
+        then:"The result is correct"
+        tenantIds == [moreBooks:2, books:0]
+
+        cleanup:
+        TransactionSynchronizationManager.unbindResource(sessionFactory)
+    }
+
+
 }
+
+
+@Entity
+class MultiTenantAuthor implements GormEntity<MultiTenantAuthor>,MultiTenant<MultiTenantAuthor> {
+    Long id
+    Long version
+    String tenantId
+    String name
+
+    static constraints = {
+        name blank:false
+    }
+}
+
