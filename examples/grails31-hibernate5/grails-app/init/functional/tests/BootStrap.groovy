bf1d3631a22a26de470a63b52b99ff1d8d7de2c4
graemerocher <graeme.rocher@gmail.com>
2017-01-19 17:23:17 +0100

    Test for #17

--- examples/grails31-hibernate5/grails-app/init/functional/tests/BootStrap.groovy
+++ examples/grails31-hibernate5/grails-app/init/functional/tests/BootStrap.groovy
@@ -1,10 +1,15 @@
 package functional.tests
 
 import functional.tests.Product
+import org.grails.orm.hibernate.HibernateDatastore
+import org.springframework.beans.factory.annotation.Autowired
 
 class BootStrap {
 
+    HibernateDatastore hibernateDatastore
+
     def init = { servletContext ->
+        assert hibernateDatastore.connectionSources.defaultConnectionSource.settings.hibernate.getConfigClass() == CustomHibernateMappingContextConfiguration
         new Product(name: "MacBook", price: "1200.01").save(flush:true)
     }
     def destroy = {
---
+++ examples/grails31-hibernate5/src/main/groovy/functional/tests/CustomHibernateMappingContextConfiguration.groovy
@@ -0,0 +1,9 @@
+package functional.tests
+
+import org.grails.orm.hibernate.cfg.HibernateMappingContextConfiguration
+
+/**
+ * Created by graemerocher on 19/01/2017.
+ */
+class CustomHibernateMappingContextConfiguration extends HibernateMappingContextConfiguration {
+}
