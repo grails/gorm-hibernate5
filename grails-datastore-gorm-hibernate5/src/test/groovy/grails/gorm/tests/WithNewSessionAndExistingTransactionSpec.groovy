cd81c2f9467d1fae6f31f0f1bde8278279460443
graemerocher <graeme.rocher@gmail.com>
2017-02-03 13:02:55 +0100

    Fix handling of withNewSession. Fixes https://github.com/grails/grails-core/issues/10448

--- grails-datastore-gorm-hibernate5/src/test/groovy/grails/gorm/tests/WithNewSessionAndExistingTransactionSpec.groovy
+++ grails-datastore-gorm-hibernate5/src/test/groovy/grails/gorm/tests/WithNewSessionAndExistingTransactionSpec.groovy
@@ -4,6 +4,7 @@ import org.grails.orm.hibernate.GormSpec
 import org.hibernate.Session
 import org.springframework.orm.hibernate5.SessionHolder
 import org.springframework.orm.hibernate5.SpringSessionSynchronization
+import org.springframework.transaction.TransactionStatus
 import org.springframework.transaction.support.TransactionSynchronizationManager
 import spock.lang.Issue
 
@@ -20,8 +21,10 @@ class WithNewSessionAndExistingTransactionSpec extends GormSpec {
     void "Test withNewSession when an existing transaction is present"() {
         when:"An existing transaction not to pick up the current session"
         sessionFactory.currentSession
+        SessionHolder previousSessionHolder = TransactionSynchronizationManager.getResource(sessionFactory)
         Book.withNewSession { Session session ->
             // access the current session
+            assert !previousSessionHolder.is(TransactionSynchronizationManager.getResource(sessionFactory))
             session.sessionFactory.currentSession
         }
         // reproduce session closed problem
@@ -29,6 +32,7 @@ class WithNewSessionAndExistingTransactionSpec extends GormSpec {
         SessionHolder sessionHolder = TransactionSynchronizationManager.getResource(sessionFactory)
 
         then:"The result is correct"
+        sessionHolder.is(previousSessionHolder)
         TransactionSynchronizationManager.isSynchronizationActive()
         sessionHolder.session.isOpen()
         sessionHolder.isSynchronizedWithTransaction()
@@ -41,11 +45,14 @@ class WithNewSessionAndExistingTransactionSpec extends GormSpec {
     void "Test with withNewSession with nested transaction"() {
         when:"An existing transaction not to pick up the current session"
         sessionFactory.currentSession
+        SessionHolder previousSessionHolder = TransactionSynchronizationManager.getResource(sessionFactory)
         Book.withNewSession { Session session ->
+            assert !previousSessionHolder.is(TransactionSynchronizationManager.getResource(sessionFactory))
             // access the current session
             session.sessionFactory.currentSession
             // reproduce "Pre-bound JDBC Connection found!" problem
             Book.withNewTransaction {
+                assert !previousSessionHolder.is(TransactionSynchronizationManager.getResource(sessionFactory))
                 new Book(title: "The Stand", author: 'Stephen King').save()
             }
         }
@@ -54,6 +61,34 @@ class WithNewSessionAndExistingTransactionSpec extends GormSpec {
         SessionHolder sessionHolder = TransactionSynchronizationManager.getResource(sessionFactory)
 
         then:"The result is correct"
+        sessionHolder.is(previousSessionHolder)
+        TransactionSynchronizationManager.isSynchronizationActive()
+        sessionHolder.session.isOpen()
+        sessionHolder.isSynchronizedWithTransaction()
+        sessionFactory.currentSession.isOpen()
+    }
+
+    @Issue('https://github.com/grails/grails-core/issues/10448')
+    void "Test with withNewSession with existing transaction"() {
+        when:"An existing transaction not to pick up the current session"
+        sessionFactory.currentSession
+        SessionHolder previousSessionHolder = TransactionSynchronizationManager.getResource(sessionFactory)
+        Book.withNewTransaction { TransactionStatus status ->
+            // reproduce "java.lang.IllegalStateException: No value for key" problem
+            Book.withNewSession { Session session ->
+                // access the current session
+                assert !previousSessionHolder.is(TransactionSynchronizationManager.getResource(sessionFactory))
+                session.sessionFactory.currentSession
+
+                new Book(title: "The Stand", author: 'Stephen King').save()
+            }
+        }
+
+        Book.count()
+        SessionHolder sessionHolder = TransactionSynchronizationManager.getResource(sessionFactory)
+
+        then:"The result is correct"
+        sessionHolder.is(previousSessionHolder)
         TransactionSynchronizationManager.isSynchronizationActive()
         sessionHolder.session.isOpen()
         sessionHolder.isSynchronizedWithTransaction()
