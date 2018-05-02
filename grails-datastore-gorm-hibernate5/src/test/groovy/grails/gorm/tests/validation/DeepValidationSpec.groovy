2594794e2b10ccefc84a626d3f826f09c37099f1
Puneet Behl <behl4485@gmail.com>
2018-05-02 15:54:14 +0530

    grails/grails-data-mapping#1033 Setting deepValidate to false will not skip validation before flush so updating tests
    The correct way is to save the child object with validate set to false so that while flushing the session it will not validate the child again.

--- grails-datastore-gorm-hibernate5/src/test/groovy/grails/gorm/tests/validation/DeepValidationSpec.groovy
+++ grails-datastore-gorm-hibernate5/src/test/groovy/grails/gorm/tests/validation/DeepValidationSpec.groovy
@@ -21,19 +21,22 @@ class DeepValidationSpec extends GormDatastoreSpec {
     void "performs deep validation correctly"() {
 
         when: "save market with failing custom validator on child"
-        new Market(name: "Main", address: new Address(streetName: "Main St.", landmark: "The Golder Gate Bridge", postalCode: "11")).save(deepValidate: false)
+        Address address = new Address(streetName: "Main St.", landmark: "The Golder Gate Bridge", postalCode: "11").save(validate: false)
+        new Market(name: "Main", address: address).save(deepValidate: false)
 
         then: "market is saved, no validation error"
         Market.count() == 1
 
         when: "save market with nullable on child"
-        new Market(name: "NIT", address: new Address(landmark: "1B, Main St.", postalCode: "121001")).save(deepValidate: false)
+        address = new Address(landmark: "1B, Main St.", postalCode: "121001").save(validate: false)
+        new Market(name: "NIT", address: address).save(deepValidate: false)
 
         then:
         thrown(DataIntegrityViolationException)
 
         when: "nested validation fails"
-        new City(name: "Faridabad").addToMarkets(name: "NIT 1", address: new Address(streetName: "1B, Main St.", landmark: "V2", postalCode: "11")).save(deepValidate: false)
+        address = new Address(streetName: "1B, Main St.", landmark: "V2", postalCode: "11").save(validate: false)
+        new City(name: "Faridabad").addToMarkets(name: "NIT 1", address: address).save(deepValidate: false)
 
         then: "market is saved, no validation error"
         City.count() == 1
