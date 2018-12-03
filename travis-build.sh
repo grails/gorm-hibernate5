7026373ffa0777682926b05928953c5eef681924
sdelamo <sergio.delamo@softamo.com>
2018-12-03 08:26:02 +0100

    prefix no-daemon with double hyphen e.g --no-daemon

--- travis-build.sh
+++ travis-build.sh
@@ -8,12 +8,12 @@ if [[ $TRAVIS_TAG =~ ^v[[:digit:]] ]]; then
 	echo "Skipping tests to Publish release"
 	./travis-publish.sh || EXIT_STATUS=$?
 else
-	./gradlew check --refresh-dependencies -no-daemon -x gorm-hibernate5-spring-boot:test  || EXIT_STATUS=$?
+	./gradlew check --refresh-dependencies --no-daemon -x gorm-hibernate5-spring-boot:test  || EXIT_STATUS=$?
 	# if [[ $EXIT_STATUS -eq 0 ]]; then
 	#     ./gradlew grails2-plugin:test -no-daemon || EXIT_STATUS=$?
 	# fi
 	if [[ $EXIT_STATUS -eq 0 ]]; then 
-	    ./gradlew gorm-hibernate5-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
+	    ./gradlew gorm-hibernate5-spring-boot:test --refresh-dependencies --no-daemon || EXIT_STATUS=$?
 	fi
 
 	./gradlew --stop
