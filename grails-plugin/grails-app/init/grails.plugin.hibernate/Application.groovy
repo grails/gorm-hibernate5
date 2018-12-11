5419ed15430892fc05b9f6e8e84d6f312a850c02
Graeme Rocher <graeme.rocher@gmail.com>
2018-12-11 16:20:52 +0100

    Build plugin against Grails 3.3.x to avoid circular dependency

--- grails-plugin/grails-app/init/grails.plugin.hibernate/Application.groovy
+++ grails-plugin/grails-app/init/grails.plugin.hibernate/Application.groovy
@@ -5,10 +5,9 @@ import grails.boot.config.GrailsAutoConfiguration
 import grails.plugins.metadata.PluginSource
 import groovy.transform.CompileStatic
 
-@CompileStatic
 @PluginSource
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
-        GrailsApp.run(Application, args)
+        GrailsApp.run((Class)Application, args)
     }
 }
