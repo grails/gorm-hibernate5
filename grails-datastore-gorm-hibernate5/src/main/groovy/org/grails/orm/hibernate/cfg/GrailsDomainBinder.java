6ffcbc5dab6aadbd4c454a0c721f4aad741ce4c3
Felix Scheinost <fesc@symentis.com>
2020-11-10 09:58:09 +0100

    Fixes composite identifier not visible to JPA
    I am not sure if this fix is correct.
    
    The hibernate documentation [1] says that there are the following ways to define a composite identity:
    
    - @EmbeddedId
    - @IdClass
    - Composite identifiers with associations => Use @Id multiple times
    
    I am not sure what the binding by `bindCompositeId` corresponds to.
    
    I just know that in the 3rd case hibernate does set the `identifierMapper`. I am not sure if there is valid in the other cases.
    
    [1]: https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#identifiers-composite

--- grails-datastore-gorm-hibernate5/src/main/groovy/org/grails/orm/hibernate/cfg/GrailsDomainBinder.java
+++ grails-datastore-gorm-hibernate5/src/main/groovy/org/grails/orm/hibernate/cfg/GrailsDomainBinder.java
@@ -1828,6 +1828,7 @@ public class GrailsDomainBinder implements MetadataContributor {
         Component id = new Component(metadataBuildingContext, root);
         id.setNullValue("undefined");
         root.setIdentifier(id);
+        root.setIdentifierMapper(id);
         root.setEmbeddedIdentifier(true);
         id.setComponentClassName(domainClass.getName());
         id.setKey(true);
